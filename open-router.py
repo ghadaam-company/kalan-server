fb7a29b608640b49443d8097a6937241a2f5cb5b
sahandmohammadrezaii <139042771+sahandmohammadrehzaii@users.noreply.github.com>
2024-05-20 10:54:50 +0330

    Update open-router.py

--- open-router.py
+++ open-router.py
@@ -1,19 +1,4 @@
-# Simon Game on BBC micro:bit V2
-# by Alan Wang
 
-"""
-How to play:
-
-1) Upload the script as main.py or in the form of .hex file.
-2) After the welcome effects, press A + B to start.
-3) Memorize the Simon sequence in each turn.
-4) Tilt the board toward indicated directions to match the sequence.
-5) After each tilting gesture, you have to return it to horizontal position.
-6) Game will end when you failed.
-7) There is no limit how long the Simon sequence can go.
-"""
-
-# import modules
 
 from microbit import display, Image, Sound, sleep
 from microbit import button_a, button_b, accelerometer
